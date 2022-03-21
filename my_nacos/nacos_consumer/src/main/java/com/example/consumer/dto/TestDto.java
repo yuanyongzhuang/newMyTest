b1a61f4b02863f134f249f2f08cbc81de8ad8e8c
yuanyongzhuang <yuanyongzhuang>
2022-03-21 14:08:23 +0800

    putifabsent

---
+++ my_nacos/nacos_consumer/src/main/java/com/example/consumer/dto/TestDto.java
@@ -0,0 +1,15 @@
+package com.example.consumer.dto;
+
+import lombok.Data;
+
+/**
+ * test
+ * @author yyz
+ */
+@Data
+public class TestDto {
+    private String order0;
+    private String order1;
+    private String order2;
+    private String order3;
+}
